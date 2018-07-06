f90da5c416d1be612dc574066af40f4b8b116216
GinoTaipe <gino_gjtm@hotmail.com>
2018-07-06 02:16:09 -0500

    Add files via upload

---
+++ Codigo Fuente/vista/V_seguimiento.php
@@ -0,0 +1,219 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Garras | Pagina Oficial</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+	<link rel="shortcut icon" href="../assets/icono_pestania.ico">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/bootstrap.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/magnific-popup.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/fonts/flaticon/font/flaticon.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="../assets/PaginaOficial/js/modernizr-2.6.2.min.js"></script>
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/owl.theme.default.min.css">
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="../assets/PaginaOficial/css/bootstrap-datepicker.css">
+
+	<script src="../assets/PaginaOficial/js/jquery.min.js"></script>
+
+
+	
+
+</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+	<div id="page">
+	<!-- Navegacion -->
+	<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="../index.php">Empresa Garras</a></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li><a href="../index.php"><i class="icon-home"></i> Inicio</a></li>
+								<li><a href="../vista/V_catalogo_productos.php"><i class="icon-tag"></i> Productos</a></li>
+								<li><a href="../vista/V_contacto.php"> <i class="icon-phone"></i> Contactanos</a></li>
+								<li class="active"><a href="../controlador/index_pedido.php"><i class="icon-shopping-cart"></i> Pedidos </a></li>
+								<?php 
+								//session_start();
+								include "../modelo/conectar_bd.php";
+							    if (isset($_SESSION['user'])){ // vlaida si se ha iniciado sesión
+								?>
+
+								<li class="has-dropdown">
+									<a href=""><i class="icon-user2"></i> <?php echo $_SESSION['user']?> </a>
+									<ul class="dropdown">
+										<?php
+											if($_SESSION['privilegio']== 1){
+										?>
+										<li><a href="../vista/Tablero_Admi.php""><i class="icon-user-plus2">Administrador</i></a></li>
+										<?php 
+										}	
+										?>
+										<li><a href="../controlador/c_validar_logout.php">Cerrar Sesión</a></li>
+									</ul>
+								</li>	
+								<?php 
+								} else { ?>
+								<li><a href="../vista/V_login.php"><i class="icon-user2"></i> Inicio Sesión </a></li>
+								<?php } ?>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+	</nav>
+	<div class="col-md-6 col-md-offset-3 text-center colorlib-heading">
+			<h2><span> Seguimiento </span></h2>
+
+		</div>
+
+</div>
+<?php  
+	if(isset($_SESSION['user'])){
+	date_default_timezone_set('America/Lima');
+
+	//Fecha de hoy
+	$fechaActual = date("Y-m-d");
+	//Fecha en la cual se realizo el pedido
+	$fechaOrigen="2018-07-05"; //Obtener, no sé como hacerlo
+	//Fecha en la cual se entregara el pedido realizado
+	$fechaEntrega = "2018-07-16"; //Obtener, no sé como hacerlo
+	$segundosFechaActual = strtotime($fechaActual);
+	$segundosFechaEntrega = strtotime($fechaEntrega);
+	$segundosFechaOrigen = strtotime($fechaOrigen);
+	$segundosFaltantes = $segundosFechaEntrega-$segundosFechaActual;
+	$segundosOrigen= $segundosFechaEntrega-$segundosFechaOrigen;
+	$diasFaltantes = $segundosFaltantes / 86400;
+	$diasTotales=$segundosOrigen/86400;
+	$diasTranscurridos=$diasTotales-$diasFaltantes;
+	echo "<h2 align=center>Faltan $diasFaltantes dias</h2>";
+
+
+	if($diasTranscurridos>$diasTotales)
+	{
+		$porcentaje=100;
+	}else
+	{
+		$porcentaje=round($diasTranscurridos*100/$diasTotales);		
+	}
+?>
+	<div class="col-md-12">
+		<div class="progress">
+			<div class="progress-bar progress-bar-striped active" role="progressbar" style="width: <?php echo $porcentaje ?>%" ><?php echo $porcentaje."% completado" ?></div>
+			
+		</div>
+	</div>
+
+	<?php  
+	}else
+	{	
+		echo "<h3 align=center>Iniciar sesión para observar el seguimiento</h3>";
+	}
+	?>
+
+
+
+		<footer id="colorlib-footer" role="contentinfo">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3 colorlib-widget">
+						<h4>Sobre nuestra tienda</h4>
+						<p>Acercate a nuestro local ubicado en Gamarrita Huanca y conoce más de nuestros productos. Te esperamos.</p>
+						<p>
+						</p>
+					</div>
+
+					<div class="col-md-3 colorlib-widget">
+						<h4>Información</h4>
+						<p>
+							<ul class="colorlib-footer-links">
+								<li><a href="../vista/V_contacto.php">Sobre nosotros</a></li>
+								<li><a href="../vista/V_terminos_condiciones.php">Términos y Condiciones</a></li>
+							</ul>
+						</p>
+					</div>
+
+					<div class="col-md-3 ">
+						<h4>Soporte</h4>
+						<ul class="colorlib-footer-links">
+							<span class="icon-file-text3"></span><a href="../assets/pdf/manual_usuario.php" target="_blank"> Manual de Usuario</a>
+						</ul>
+					</div>
+
+					<div class="col-md-3">
+						<h4>Información de contacto</h4>
+						<ul class="colorlib-footer-links">
+							<li>Jr. Huanuco N° 248 <br> Huancayo </li>
+							<li>935993138 </li>
+							<li>iromega@hotmail.com</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="copy">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<p>
+							<span class="block"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados </span> 
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<!--<ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+            <i class="fa fa-fw fa-sign-out"></i>Cerrar Sesión</a>
+        </li>
+      </ul>-->
+	
+	<!-- jQuery -->
+	<script src="../assets/PaginaOficial/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="../assets/PaginaOficial/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="../assets/PaginaOficial/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="../assets/PaginaOficial/js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="../assets/PaginaOficial/js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="../assets/PaginaOficial/js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="../assets/PaginaOficial/js/jquery.magnific-popup.min.js"></script>
+	<script src="../assets/PaginaOficial/js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="../assets/PaginaOficial/js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="../assets/PaginaOficial/js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="../assets/PaginaOficial/js/main.js"></script>
+
+	</body>
+</html>
